7bc19dbdf612dd61aec3300082ddc9a8d35ddbb8
Pierre Liduena <assurancesvelo@gmail.com>
2017-10-21 10:26:46 +0200

    e
    e

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20171018090952) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cyclos", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.string "date"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "etape"
+  end
+
+  create_table "lots", force: :cascade do |t|
+    t.string "image"
+    t.string "brand"
+    t.string "description"
+    t.string "recompense"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "marques", force: :cascade do |t|
+    t.string "logo"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recomandations", force: :cascade do |t|
+    t.string "name"
+    t.string "statut"
+    t.string "view"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo"
+  end
+
+end
